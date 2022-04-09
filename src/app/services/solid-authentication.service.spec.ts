3124b49ddb61871a6c159fb65d614240b46b87c0
Miriam Gehbauer <e11708473@student.tuwien.ac.at>
2022-04-09 15:11:50 +0200

    test: first tests for AuthenticationService.Ref#18

--- src/app/services/solid-authentication.service.spec.ts
+++ src/app/services/solid-authentication.service.spec.ts
@@ -16,4 +16,55 @@ describe('SolidAuthenticationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('initializeLoginStatus change isInitialized to true ', async () => {
+    expect(service['isInitialized']).toBeFalse();
+    await service.initializeLoginStatus();
+    expect(service['isInitialized']).toBeTrue();
+  });
+
+  it('initializeLoginStatus does not chance initializedCallbacks', async () => {
+    expect(service['initializedCallbacks']).toEqual([]);
+    await service.initializeLoginStatus();
+    expect(service['initializedCallbacks']).toEqual([]);
+  });
+
+  it('initializeLoginStatus call onLoginStatusKnown', async () => {
+    const onLoginStatusKnownSpy = spyOn<any>(service, 'onLoginStatusKnown');
+    await service.initializeLoginStatus();
+    expect(onLoginStatusKnownSpy).toHaveBeenCalled();
+  });
+
+  it('onLoginStatusKnown change isInitialized to true ', async () => {
+    expect(service['isInitialized']).toBeFalse();
+    service['onLoginStatusKnown']();
+    expect(service['isInitialized']).toBeTrue();
+  });
+
+  it('onLoginStatusKnown does not chance initializedCallbacks', async () => {
+    expect(service['initializedCallbacks']).toEqual([]);
+    service['onLoginStatusKnown']();
+    expect(service['initializedCallbacks']).toEqual([]);
+  });
+
+  it('isLoggedIn call waitUntilInitialized', async () => {
+    const waitUntilInitializedSpy = spyOn<any>(service, 'waitUntilInitialized');
+    await service.isLoggedIn();
+    expect(waitUntilInitializedSpy).toHaveBeenCalled();
+  });
+
+  it('isLoggedIn call isStoredLoggedIn', async () => {
+    spyOn<any>(service, 'waitUntilInitialized');
+    const isStoredLoggedInSpy = spyOn<any>(service, 'isStoredLoggedIn');
+    await service.isLoggedIn();
+    expect(isStoredLoggedInSpy).toHaveBeenCalled();
+  });
+
+  it('waitUntilInitialized should return undefined if isInitialized is true', async () => {
+    expect(service['isInitialized']).toBeFalse();
+    service['onLoginStatusKnown']();
+    expect(service['isInitialized']).toBeTrue();
+
+    expect(await service['waitUntilInitialized']()).toBeUndefined();
+  });
 });
