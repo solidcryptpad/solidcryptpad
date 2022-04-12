afa4842492e64cb2650a75045d333d3fd57e7670
HED <dallinger.hannes@gmail.com>
2022-04-12 13:27:44 +0200

    docs: added javadoc for writing and reading files. Ref #21

--- src/app/services/file_handler/solid-file-handler.service.ts
+++ src/app/services/file_handler/solid-file-handler.service.ts
@@ -6,12 +6,26 @@ import { fetch } from '@inrupt/solid-client-authn-browser';
   providedIn: 'root',
 })
 export class SolidFileHandlerService {
+  /**
+   * reads a file saved at the url
+   *
+   * @param fileURL the url to read from
+   * @returns a promise for the file as a blob
+   */
   async readFile(fileURL: string): Promise<Blob> {
     return await getFile(fileURL, {
       fetch: fetch,
     });
   }
 
+  /**
+   * writes a file to an url
+   * if the file already exists then it is overwritten
+   * if the file does not exist then a new one is created
+   *
+   * @param fileURL the url to write to
+   * @returns a promise for the saved file
+   */
   async writeFile(file: Blob, fileURL: string): Promise<Blob> {
     return await overwriteFile(fileURL, file, {
       contentType: file.type,
