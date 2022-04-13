9107fadc8edc4c022cb8ca6ed83e11042618f8a9
phi777 <phi777@protonmail.com>
2022-04-13 19:51:38 +0200

    feat: added basic collaborative editing

--- src/app/components/text-editor/text-editor.component.ts
+++ src/app/components/text-editor/text-editor.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Editor } from 'ngx-editor';
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import {
+  ySyncPlugin,
+  yCursorPlugin,
+  yUndoPlugin,
+  undo,
+  redo,
+} from 'y-prosemirror';
+import { keymap } from 'prosemirror-keymap';
 
 @Component({
   selector: 'app-text-editor',
@@ -11,7 +21,28 @@ export class TextEditorComponent implements OnInit, OnDestroy {
   html: '' = '';
 
   ngOnInit(): void {
-    this.editor = new Editor();
+    const ydoc = new Y.Doc();
+    // clients connected to the same room-name share document updates
+    const provider = new WebrtcProvider('your-room-name', ydoc);
+    //const yarray = ydoc.get('array', Y.Array);
+
+    const type = ydoc.getXmlFragment('prosemirror');
+
+    //https://github.com/yjs/y-prosemirror#utilities zum speichern
+
+    this.editor = new Editor({
+      history: false, // include the history plugin manually
+      plugins: [
+        ySyncPlugin(type),
+        yCursorPlugin(provider.awareness),
+        yUndoPlugin(),
+        keymap({
+          'Mod-z': undo,
+          'Mod-y': redo,
+          'Mod-Shift-z': redo,
+        }),
+      ],
+    });
   }
 
   ngOnDestroy(): void {
