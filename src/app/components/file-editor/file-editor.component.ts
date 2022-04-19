164d1738715c603ba5f728f666f1b0489e6611c3
HED <dallinger.hannes@gmail.com>
2022-04-19 20:15:22 +0200

    feat: added locale file upload. Ref#21

--- src/app/components/file-editor/file-editor.component.ts
+++ src/app/components/file-editor/file-editor.component.ts
@@ -7,11 +7,16 @@ import { NotificationService } from 'src/app/services/notification/notification.
   templateUrl: './file-editor.component.html',
   styleUrls: ['./file-editor.component.scss'],
 })
+/**
+ * This component is a simple example for uploading and downloading and will be replaced in the future
+ */
 export class FileEditorComponent {
   fileContent = 'no content';
   newFileContent = '';
   link = '';
   newlink = '';
+  uploadLink = ''; // link the new file will be uploaded to
+  file: FileList = { length: 0, item: () => null }; // list of uploaded files
 
   constructor(
     private solidFileHandler: SolidFileHandlerService,
@@ -34,6 +39,68 @@ export class FileEditorComponent {
     try {
       const blob = new Blob([this.newFileContent], { type: 'text/plain' });
       await this.solidFileHandler.writeFile(blob, link);
+    } catch (error: any) {
+      this.notificationService.error({
+        title: error.title,
+        message: error.message,
+      });
+    }
+  }
+
+  selectFile(event: any) {
+    if (event.target == null) return;
+    else {
+      this.file = event.target.files;
+
+      if (event.target.files.length != 1) {
+        this.notificationService.info({
+          title: 'multiple files choosen',
+          message: 'they will be uploaded as a folder',
+        });
+      }
+    }
+  }
+
+  /**
+   * uploads a file to the given pod
+   * @param file the file to upload
+   * @param link the link to the solid pod
+   */
+  async uploadFile(file: File, link: string): Promise<void> {
+    console.log(
+      await (await this.solidFileHandler.writeFile(file, link)).text()
+    );
+    this.notificationService.success({
+      title: 'upload',
+      message: 'success',
+    });
+  }
+
+  /**
+   * uploads multiple files to a solid pod
+   * @param files the files to upload
+   * @param link the link to upload them
+   */
+  async uploadFiles(files: FileList, link: string): Promise<void> {
+    try {
+      if (files.length == 0) {
+        this.notificationService.info({
+          title: 'no file selected',
+          message: 'please select one or more files',
+        });
+      } else if (files.length == 1) {
+        await this.uploadFile(files[0], link);
+      } else {
+        // this is currently untested because my browser-setup seems to not allow to upload more then one file
+        // will change anyway as soon as the overwriteFile bug workaround is in
+        for (let i = 0; i < files.length; i++) {
+          let flink;
+          if (link.endsWith('/')) flink = link + `${files[0].name}`;
+          else flink = link + `/${files[0].name}`;
+
+          await this.uploadFile(files[i], flink);
+        }
+      }
     } catch (error: any) {
       this.notificationService.error({
         title: error.title,
