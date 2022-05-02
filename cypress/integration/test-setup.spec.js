efece675e9f40afb32bf637f368942b2ed727868
Miriam Gehbauer <e11708473@student.tuwien.ac.at>
2022-05-02 14:48:33 +0200

    test: add community-solid-server and add Cypress Commands createRandomAccount and Login. Ref #29

---
+++ cypress/integration/test-setup.spec.js
@@ -0,0 +1,15 @@
+describe("Test setup", function () {
+  it("can use createRandomAccount", function () {
+    cy.createRandomAccount().then(console.log);
+  });
+
+  it("can use login command", function () {
+    cy.createRandomAccount().as("user");
+    //cy.visit('localhost:4200/')
+    cy.get("@user").then(function (user) {
+      cy.login(user);
+    });
+    cy.visit("localhost:4200/home");
+    cy.contains("hello authenticated user!");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,61 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("createRandomAccount", function () {
+  var uuid = require("uuid");
+  var username = "test-" + uuid.v4();
+  var password = "12345";
+  var email = username + "@example.org";
+  var config = {
+    idp: Cypress.config().cssUrl + "/",
+    podUrl: Cypress.config().cssUrl + "/" + username,
+    webId: Cypress.config().cssUrl + "/" + username + "/profile/card#me",
+    username: username,
+    password: password,
+    email: email,
+  };
+  var registerEndpoint = Cypress.config().cssUrl + "/idp/register/";
+  cy.request("POST", registerEndpoint, {
+    createWebId: "on",
+    webId: "",
+    register: "on",
+    createPod: "on",
+    podName: username,
+    email: email,
+    password: password,
+    confirmPassword: password,
+  });
+
+  return cy.wrap(config);
+});
+
+/**
+ * Manually logins the user
+ * Assumes it is previously not logged in
+ */
+Cypress.Commands.add("login", function (user) {
+  var typeFastConfig = {
+    delay: 0,
+  };
+  //cy.clearLocalStorage()
+  cy.log("login", user);
+  cy.visit("localhost:4200/");
+
+  cy.get("input").type(Cypress.config().cssUrl + "/");
+  cy.contains("LOGIN").click();
+
+  cy.url().should("include", user.idp);
+
+  cy.get("label").contains("Email").click().type(user.email, typeFastConfig);
+  cy.get("label")
+    .contains("Password")
+    .click()
+    .type(user.password, typeFastConfig);
+  cy.contains("button", "Log in").click();
+
+  cy.url().should("include", "/consent");
+  cy.contains("button", "Consent").click();
+
+  cy.url().should("include", Cypress.config().baseUrl + "/");
+});
