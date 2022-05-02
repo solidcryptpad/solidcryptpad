450c336041868c8d27cff1939e841ec622252d3e
phi777 <phi777@protonmail.com>
2022-05-02 19:39:55 +0200

    feat: filename as url parameter, close file button, links to open example files, added editor to navbar

--- src/app/components/text-editor/text-editor.component.ts
+++ src/app/components/text-editor/text-editor.component.ts
@@ -14,6 +14,8 @@ import { ProfileService } from '../../services/profile/profile.service';
 import { SolidFileHandlerService } from '../../services/file_handler/solid-file-handler.service';
 import { YXmlFragment } from 'yjs/dist/src/types/YXmlFragment';
 import { fromEvent, debounceTime } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotFoundException } from '../../exceptions/not-found-exception';
 
 @Component({
   selector: 'app-text-editor',
@@ -26,26 +28,47 @@ export class TextEditorComponent implements OnInit, OnDestroy {
   readyForSave = false;
   xmlFragement: YXmlFragment | undefined;
   baseUrl = '';
+  filename = '';
+  provider!: WebrtcProvider;
+  ydoc!: Y.Doc;
 
   constructor(
     private profileService: ProfileService,
-    private fileService: SolidFileHandlerService
+    private fileService: SolidFileHandlerService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    const ydoc = new Y.Doc();
-    // clients connected to the same room-name share document updates
-    const provider = new WebrtcProvider('your-room-name', ydoc);
+    this.route.queryParams.subscribe((params) => {
+      this.filename = params['file'];
+      if (
+        this.filename === null ||
+        this.filename === undefined ||
+        this.filename === ''
+      ) {
+        console.debug('no filename given');
+      } else {
+        this.setupEditor();
+      }
+    });
+  }
 
-    this.xmlFragement = ydoc.getXmlFragment('prosemirror');
+  /**
+   * prepares the editor for the current file
+   * https://github.com/yjs/y-prosemirror#utilities
+   */
+  setupEditor(): void {
+    this.ydoc = new Y.Doc();
+    this.provider = new WebrtcProvider(this.getRoomName(), this.ydoc);
 
-    //https://github.com/yjs/y-prosemirror#utilities zum speichern
+    this.xmlFragement = this.ydoc.getXmlFragment('prosemirror');
 
     this.editor = new Editor({
       history: false, // include the history plugin manually
       plugins: [
         ySyncPlugin(this.xmlFragement),
-        yCursorPlugin(provider.awareness),
+        yCursorPlugin(this.provider.awareness),
         yUndoPlugin(),
         keymap({
           'Mod-z': undo,
@@ -59,7 +82,7 @@ export class TextEditorComponent implements OnInit, OnDestroy {
       this.baseUrl = podUrls[0];
       console.debug('using URL:  ' + this.getUrl());
 
-      const updateYdoc = fromEvent(ydoc, 'update');
+      const updateYdoc = fromEvent(this.ydoc, 'update');
       const result = updateYdoc.pipe(debounceTime(1000));
       result.subscribe(() => {
         if (!this.readyForSave) {
@@ -95,19 +118,56 @@ export class TextEditorComponent implements OnInit, OnDestroy {
         });
       },
       (reason) => {
+        if (reason instanceof NotFoundException) {
+          console.debug('file not found, creating file');
+          this.readyForSave = true;
+          this.saveFile();
+          return;
+        }
         console.error('couldnt load file: ' + reason);
       }
     );
   }
 
   /**
+   * closes the current file with saving it
+   */
+  closeFile(): void {
+    this.readyForSave = false;
+    this.saveFile();
+    this.closeEditor();
+    this.html = '';
+    this.router.navigate(['/editor'], { queryParams: { filename: '' } });
+  }
+
+  /**
+   * closes editor and ydoc
+   */
+  closeEditor(): void {
+    this.provider?.disconnect();
+    this.ydoc?.destroy();
+    this.editor?.destroy();
+  }
+
+  /**
    * @return the url for the current opened file
    */
   getUrl(): string {
-    return this.baseUrl + 'private/solidcryptpad/' + 'doc0.txt';
+    return this.baseUrl + 'private/solidcryptpad/' + this.filename;
+  }
+
+  /**
+   * @return the room name for the current opened file
+   */
+  getRoomName(): string {
+    return 'room-' + this.getUrl(); //TODO generate room name
+  }
+
+  getRoomPassword(): string {
+    return 'pw-' + this.getUrl(); //TODO generate room pw
   }
 
   ngOnDestroy(): void {
-    this.editor.destroy();
+    this.closeEditor();
   }
 }
