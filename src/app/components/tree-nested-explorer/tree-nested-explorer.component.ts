28b34be0755987efa7a3db1c7e001c6e669b40c8
HED <dallinger.hannes@gmail.com>
2022-05-03 18:50:37 +0200

    feat: make folders openable anddisplay the content. Ref #43

--- src/app/components/tree-nested-explorer/tree-nested-explorer.component.ts
+++ src/app/components/tree-nested-explorer/tree-nested-explorer.component.ts
@@ -1,6 +1,7 @@
 import {
   CollectionViewer,
   DataSource,
+  SelectionChange,
 } from '@angular/cdk/collections';
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { Component } from '@angular/core';
@@ -8,41 +9,95 @@ import { BehaviorSubject, merge, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SolidFileHandlerService } from 'src/app/services/file_handler/solid-file-handler.service';
 
-export class DynamicFlatNode {
+export class Node {
   constructor(
-    public item: string,
-    public level = 1,
-    public expandable = false,
-    public isLoading = false
+    public item: string, //link to the folder in a pod
+    public level: number = 1, // how deep is it from root
+    public expandable: boolean = true, // is it a folder and therefor can it be opened
+    public isLoading: boolean = false // is that object currently busy loading data
   ) {}
 }
 
-export class DynamicDataSource implements DataSource<DynamicFlatNode> {
-  dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
-  root = 'https://hed.solidweb.org/';
+export class FolderDataSource implements DataSource<Node> {
+  dataChange = new BehaviorSubject<Node[]>([]);
+
+  get data(): Node[] {
+    return this.dataChange.value;
+  }
+
   constructor(
-    private treeControl: FlatTreeControl<DynamicFlatNode>,
-    public solidFileHandlerService: SolidFileHandlerService
+    private treeControl: FlatTreeControl<Node>,
+    public solidFileHandlerService: SolidFileHandlerService,
+    public root: string
   ) {
-    solidFileHandlerService.getContainerContent(this.root).then((x) => {
-      const data: DynamicFlatNode[] = [];
+    solidFileHandlerService.getContainerContent(root).then((x) => {
+      const data: Node[] = [];
       x.forEach((element) => {
-        data.push(new DynamicFlatNode(element.substring(this.root.length)));
+        data.push(new Node(element));
       });
       this.dataChange.next(data);
     });
   }
 
-  connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
-    this.treeControl.expansionModel.changed.subscribe((change) =>
-      console.log(change)
-    );
+  connect(collectionViewer: CollectionViewer): Observable<Node[]> {
+    this.treeControl.expansionModel.changed.subscribe((event) => {
+      //this is correct that way, if the event is not supposed to open/close anything event will hold an empty list in added or removed, therefor the foreach won't do anything
+      this.openFolder(event);
+      this.closeFolder(event);
+    });
 
     return merge(collectionViewer.viewChange, this.dataChange).pipe(
-      map(() => this.dataChange.value)
+      map(() => this.data)
     );
   }
 
+  //todo check if node is actually openable
+  openFolder(event: SelectionChange<Node>): void {
+    event.added.forEach((node) => {
+      const index = this.data.indexOf(node);
+      node.isLoading = true;
+      console.log(node.item);
+      this.solidFileHandlerService
+        .getContainerContent(node.item)
+        .then((children) => {
+          const new_children: Node[] = [];
+
+          children.forEach((child) => {
+            new_children.push(new Node(child, node.level + 1, true));
+          });
+
+          this.data.splice(index + 1, 0, ...new_children);
+          node.isLoading = false;
+          this.dataChange.next(this.data);
+        });
+    });
+  }
+
+  closeFolder(event: SelectionChange<Node>): void {
+    event.removed
+      .slice()
+      .reverse()
+      .forEach((node) => {
+        node.isLoading = true;
+
+        const index = this.data.indexOf(node);
+        let count = 0;
+
+        // simply counts how many elements to remove, does not have to do anything in the body
+        for (
+          let i = index + 1;
+          i < this.data.length && this.data[i].level > node.level;
+          i++, count++
+        ) {}
+
+        this.data.splice(index + 1, count);
+
+        this.dataChange.next(this.data);
+        node.isLoading = false;
+      });
+  }
+
+  // is required for the interface but eslint does not like empty functions
   // eslint-disable-next-line
   disconnect(collectionViewer: CollectionViewer): void {}
 }
@@ -54,23 +109,30 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
 })
 export class TreeNestedExplorerComponent {
   constructor(public solidFileHandlerService: SolidFileHandlerService) {
-    this.treeControl = new FlatTreeControl<DynamicFlatNode>(
+    this.treeControl = new FlatTreeControl<Node>(
       this.getLevel,
       this.isExpandable
     );
-    this.dataSource = new DynamicDataSource(
+    this.dataSource = new FolderDataSource(
       this.treeControl,
-      solidFileHandlerService
+      solidFileHandlerService,
+      'https://hed.solidweb.org/' //todo make changeable, possibly read from pathvariable
     );
   }
 
-  treeControl: FlatTreeControl<DynamicFlatNode>;
+  treeControl: FlatTreeControl<Node>;
 
-  dataSource: DynamicDataSource;
+  dataSource: FolderDataSource;
 
-  getLevel = (node: DynamicFlatNode) => node.level;
+  getLevel(node: Node): number {
+    return node.level;
+  }
 
-  isExpandable = (node: DynamicFlatNode) => node.expandable;
+  isExpandable(node: Node): boolean {
+    return node.expandable;
+  }
 
-  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
+  hasChild(_: number, nodeData: Node) {
+    return nodeData.expandable;
+  }
 }
