544278ae52331d6ad89b0a30cf811dfee8967408
A_A <21040751+Otto-AA@users.noreply.github.com>
2022-06-15 15:26:47 +0200

    test: make text-editor e2e tests more stable

--- cypress/integration/text-editor.ts
+++ cypress/integration/text-editor.ts
@@ -1,7 +1,15 @@
 describe('Text-Editor Test', function () {
   beforeEach(function () {
-    cy.createRandomAccount().then(cy.loginMocked).as('user');
+    cy.createRandomAccount()
+      .then(cy.loginMocked)
+      .then(cy.storeMasterPassword)
+      .as('user');
     cy.contains('Editor').click();
+
+    cy.intercept('GET', '*/solidcryptpad/ExampleFile0.txt').as(
+      'fetchedExample0'
+    );
+    cy.intercept('PUT', '*/solidcryptpad/ExampleFile0.txt').as('savedExample0');
   });
 
   /**
@@ -17,7 +25,6 @@ describe('Text-Editor Test', function () {
    **/
   it('open example file and display editor', function () {
     cy.contains('ExampleFile0.txt').click();
-    cy.enterMasterPassword(this.user);
     cy.contains('solidcryptpad/ExampleFile0.txt');
     cy.contains('You are editing');
     cy.contains('Save and close');
@@ -29,27 +36,34 @@ describe('Text-Editor Test', function () {
    **/
   it('Edit file content and save it and reopen it', function () {
     cy.contains('Open ExampleFile0.txt').click();
-    cy.enterMasterPassword(this.user);
     cy.contains('solidcryptpad/ExampleFile0.txt');
+    cy.wait('@savedExample0');
     cy.get('.NgxEditor').type('Hello world!');
+    cy.wait('@savedExample0');
     cy.contains('Save and close file').click();
+    cy.wait('@savedExample0');
     cy.contains('Open ExampleFile0.txt').click();
     cy.contains('Hello world!');
   });
 
   /**
    Open a file without auto save and edit the content and close it.
-   Then reopen the file and check the content.
+   Then reopen the file and check that the file response is the same as before.
    **/
   it('Edit file content and save it and reopen it', function () {
     cy.contains('Open ExampleFile0.txt').click();
-    cy.enterMasterPassword(this.user);
-    cy.wait(500);
+    cy.wait('@fetchedExample0').its('response.body').as('initialResponseBody');
     cy.contains('Auto-Save').click();
     cy.contains('solidcryptpad/ExampleFile0.txt');
     cy.get('.NgxEditor').type('ABCDEFG');
     cy.contains('Close file without saving').click();
+
     cy.contains('Open ExampleFile0.txt').click();
-    cy.get('.NgxEditor').should('not.contain', 'ABCDEFG');
+    cy.get('@initialResponseBody').then((initialResponseBody) =>
+      cy
+        .wait('@fetchedExample0')
+        .its('response.body')
+        .should('deep.equal', initialResponseBody)
+    );
   });
 });
