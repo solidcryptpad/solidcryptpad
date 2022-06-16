fc1fea0281af86c6d4b1fbbf9d77236c043bc073
A_A <21040751+Otto-AA@users.noreply.github.com>
2022-06-16 11:49:03 +0200

    fix: search for key in multiple keystores. Ref #84
    this is necessary, because multiple keystores can be responsible
    for an url (e.g. for shared/foo.txt the root keystore and shared
    keystore). But the key could be only stored in the shared keystore,
    if it is added by a friend.

--- src/app/exceptions/key-not-found-exception.ts
+++ src/app/exceptions/key-not-found-exception.ts
@@ -3,6 +3,6 @@ import { ErrorOptions } from './error-options';
 
 export class KeyNotFoundException extends BaseException {
   constructor(message: string, options?: ErrorOptions) {
-    super('KeyNotFoundException', message, 'Decryption key not found', options);
+    super('KeyNotFoundException', message, 'Encryption key not found', options);
   }
 }
