2245cdcd95401e00a90c1c7074e16c0ff68ad5d6
Hld0 <hld@dismail.de>
2022-06-24 17:43:14 +0100

    fix: unit tests failing. Ref: #86

--- src/app/components/shared-by-me/shared-by-me.component.spec.ts
+++ src/app/components/shared-by-me/shared-by-me.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SharedByMeComponent } from './shared-by-me.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SolidAuthenticationService } from '../../services/authentication/solid-authentication.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrService } from 'ngx-toastr';
 
 describe('SharedByMeComponent', () => {
   let component: SharedByMeComponent;
@@ -8,7 +12,12 @@ describe('SharedByMeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatDialogModule, RouterTestingModule],
       declarations: [SharedByMeComponent],
+      providers: [
+        { provide: SolidAuthenticationService, useValue: {} },
+        { provide: ToastrService, useValue: {} },
+      ],
     }).compileComponents();
   });
 
