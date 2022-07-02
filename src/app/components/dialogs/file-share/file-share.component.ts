fcdcb06a4b777cab0e3b6953527266c923acba55
Hld0 <hld@dismail.de>
2022-07-02 22:43:41 +0200

    ui: slight improvement of share dialog ui.

--- src/app/components/dialogs/file-share/file-share.component.ts
+++ src/app/components/dialogs/file-share/file-share.component.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { LinkShareService } from '../../../services/link-share/link-share.service';
 import { throwWithContext } from '../../../exceptions/error-options';
+import { NotificationService } from '../../../services/notification/notification.service';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-link-share',
@@ -21,6 +23,8 @@ export class FileShareComponent {
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<FileShareComponent>,
     private linkSharingService: LinkShareService,
+    private notificationService: NotificationService,
+    private clipboard: Clipboard,
     @Inject(MAT_DIALOG_DATA) public data: { fileUrl: string }
   ) {}
 
@@ -42,5 +46,15 @@ export class FileShareComponent {
 
   cancel(): void {
     this.dialogRef.close();
+  }
+
+  async copyLink() {
+    if (this.link != null) {
+      this.clipboard.copy(this.link);
+    }
+    this.notificationService.success({
+      title: 'Success!',
+      message: 'Link has been copied to your clipboard.',
+    });
   }
 }
--- src/app/components/dialogs/folder-share/folder-share.component.ts
+++ src/app/components/dialogs/folder-share/folder-share.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormControl } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { throwWithContext } from 'src/app/exceptions/error-options';
 import { LinkShareService } from 'src/app/services/link-share/link-share.service';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { NotificationService } from '../../../services/notification/notification.service';
 
 @Component({
   selector: 'app-folder-share',
@@ -21,6 +23,8 @@ export class FolderShareComponent {
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<FolderShareComponent>,
     private linkSharingService: LinkShareService,
+    private notificationService: NotificationService,
+    private clipboard: Clipboard,
     @Inject(MAT_DIALOG_DATA) public data: { folderUrl: string }
   ) {}
 
@@ -42,5 +46,15 @@ export class FolderShareComponent {
 
   cancel(): void {
     this.dialogRef.close();
+  }
+
+  async copyLink() {
+    if (this.link != null) {
+      this.clipboard.copy(this.link);
+    }
+    this.notificationService.success({
+      title: 'Success!',
+      message: 'Link has been copied to your clipboard.',
+    });
   }
 }
