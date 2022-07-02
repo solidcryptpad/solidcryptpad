83fad8c24e3e82e58aff01de9e62d3bce12940f2
HED <dallinger.hannes@gmail.com>
2022-07-02 14:28:10 +0200

    fix: fixed redirect bug. Ref #131

--- src/app/services/redirect/redirect.service.spec.ts
+++ src/app/services/redirect/redirect.service.spec.ts
@@ -1,17 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { Routes } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 import { RedirectService } from './redirect.service';
 
 describe('RedirectService', () => {
   let service: RedirectService;
 
   beforeEach(() => {
-    const routes = [] as Routes;
-
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes(routes)],
-    });
+    TestBed.configureTestingModule({});
     service = TestBed.inject(RedirectService);
   });
 
--- src/app/services/redirect/redirect.service.ts
+++ src/app/services/redirect/redirect.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RedirectService {
   private storage_key = 'url';
-
-  constructor(private router: Router) {}
 
   public setRedirect(url: string) {
     localStorage.setItem(this.storage_key, url);
@@ -17,9 +14,10 @@ export class RedirectService {
    * redirects to url or does nothing if not set
    */
   public tryRedirect() {
-    const url = localStorage.getItem('url');
+    const url = localStorage.getItem(this.storage_key);
     if (url !== null) {
-      this.router.navigateByUrl(url);
+      localStorage.removeItem(this.storage_key);
+      window.location.href = url;
     }
   }
 }
